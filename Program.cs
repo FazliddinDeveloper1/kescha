126e51d251da38f21393ffdf6fd9e0b4985806d5
FazliddinDeveloper1 <fazliddinx0506@gmail.com>
2023-02-18 02:01:22 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+string greeting = $"Hello! {name}";
+Console.WriteLine(greeting);
+
+Console.Write("Enter your age: ");
+string ageAsString = Console.ReadLine();
+Console.WriteLine("Converting");
+int
