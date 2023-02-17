f3c218f1940a597067934dfb75a2ebcaf8e436ce
FazliddinDeveloper1 <fazliddinx0506@gmail.com>
2023-02-18 02:09:10 +0500

    INFRA: Upload Project

--- Program.cs
+++ Program.cs
@@ -6,4 +6,9 @@ Console.WriteLine(greeting);
 Console.Write("Enter your age: ");
 string ageAsString = Console.ReadLine();
 Console.WriteLine("Converting");
-int
+int age = Convert.ToInt32(ageAsString);
+Console.WriteLine($"Succesfully converted  {age}");
+
+int keschasAge = 3;
+int ageDiffernece = age - keschasAge;
+Console.WriteLine($"The difference between your and keschas age is {ageDiffernece}");
